1ac51eec3d1b93796d726ee11f6b1d1f420f1fa6
田浪 <1584731441@qq.com>
2022-02-24 22:51:26 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from './App.vue'
+app.use(ElementPlus)
+createApp(App).mount('#app')
